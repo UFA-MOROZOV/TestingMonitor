e4a3741638e94ee034e99e58601c76a4776afae2
UFA-MOROZOV <74819600+UFA-MOROZOV@users.noreply.github.com>
2026-01-05 13:35:20 +0500

    Добавлен сваггер и эндпоинт

---
+++ TestingMonitor.Application/UseCases/Compilers/Get/GetCompilersQuery.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TestingMonitor.Application.Interfaces;
+
+namespace TestingMonitor.Application.UseCases.Compilers.Get;
+
+/// <summary>
+/// Запрос на получение компиляторов.
+/// </summary>
+public sealed class GetCompilersQuery : IRequest<List<CompilerDto>>
+{
+    private sealed class Handler (IDbContext dbContext) : IRequestHandler<GetCompilersQuery, List<CompilerDto>>
+    {
+        public async Task<List<CompilerDto>> Handle(GetCompilersQuery request, CancellationToken cancellationToken)
+        {
+            var compilers = await dbContext.Compilers.ToListAsync(cancellationToken);
+
+            return compilers.Select(x => new CompilerDto
+            {
+                Id  = x.Id,
+                Name = x.Name,
+                Version = x.Version,
+            }).ToList();
+        }
+    }
+}
