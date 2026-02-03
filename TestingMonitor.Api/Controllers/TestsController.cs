46b8f1c9957cc5e3bd86e762990c3bc427b0e47f
UFA-MOROZOV <frosty.maa1@gmail.com>
2026-02-03 21:20:25 +0300

    fix

--- TestingMonitor.Api/Controllers/TestsController.cs
+++ TestingMonitor.Api/Controllers/TestsController.cs
@@ -1,13 +1,10 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using TestingMonitor.Application.UseCases.Compilers.DownloadDocker;
-using TestingMonitor.Application.UseCases.Compilers.ExecuteCode;
-using TestingMonitor.Application.UseCases.Compilers.Get;
 
 namespace TestingMonitor.Api.Controllers;
 
 [Route("/api/compilers")]
 public sealed class TestsController(IMediator mediator) : Controller
 {
-    
+
 }
