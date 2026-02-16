a033a8e8963640b720e10dd607ca220a3caba272
UFA-MOROZOV <frosty.maa1@gmail.com>
2026-02-16 11:53:34 +0500

    fix

---
+++ TestingMonitor.Infrastructure/Migrations/20260216065328_AddImageName.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestingMonitor.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageName",
+                table: "Compilers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageName",
+                table: "Compilers");
+        }
+    }
+}
